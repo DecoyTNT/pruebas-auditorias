47c1458b838ce028e90e6ebbdc608be6f652568d
Jorge Madera <decoy33tnt@hotmail.com>
2019-09-12 14:48:45 -0500

    Token headers

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken')
 // Verificar Token
 // =======================
 let verificaToken = (req, res, next) => {
-    let token = req.query.token;
+    let token = req.get('token')
 
     jwt.verify(token, process.env.SEED, (err, decoded) => {
         if (err) {
