2741f00eeddb4afad6285e44c627a6e754842fe7
Jorge Madera <decoy33tnt@hotmail.com>
2019-09-12 14:18:28 -0500

    Sin semilla

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -7,7 +7,7 @@ const jwt = require('jsonwebtoken')
 let verificaToken = (req, res, next) => {
     let token = req.get('token')
 
-    jwt.verify(token, process.env.SEED, (err, decoded) => {
+    jwt.verify(token, (err, decoded) => {
         if (err) {
             return res.status(401).json({
                 ok: false,
