8bb00b8233c5430246ebf5628e2195eeef41973c
Jorge Madera <decoy33tnt@hotmail.com>
2019-10-03 17:28:12 -0500

    ruta verificacion en proceso

--- server/routes/verificacion.js
+++ server/routes/verificacion.js
@@ -104,8 +104,8 @@ app.get('/verificacion/auditoria/:id', (req, res) => {
         })
 })
 
-// Obtener las verificaciones por auditoria y por usuario 
-app.get('/verificacion/auditoria/usuario/:id', (req, res) => {
+// Obtener las verificaciones por usuario 
+app.get('/verificacion/usuario/:id', (req, res) => {
     let usuarioid = req.params.id
 
     Usurio.findById(usuarioid)
@@ -144,6 +144,58 @@ app.get('/verificacion/auditoria/usuario/:id', (req, res) => {
                 })
         })
 })
+
+// Obtener las verificaciones por auditoria y auditor 
+app.get('/verificacion/auditoria/usuario/:idAuditoria/:idUsuario', (req, res) => {
+    let auditoriaid = req.params.idAuditoria
+    let auditorid = req.params.idUsuario
+
+    Verificacion.find({ auditoria: auditoriaid, auditor: auditorid })
+        .exec((err, verificaciones) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                })
+            }
+
+            if (!verificaciones) {
+                return res.status(400).json({
+                    ok: false,
+                    err: {
+                        message: 'No se encontro ninguna verificacion para esa auditoria o auditor'
+                    }
+                })
+            }
+
+            Verificacion.count({ auditoria: auditoriaid, auditor: auditorid }, (err, conteo) => {
+                res.json({
+                    ok: true,
+                    verificaciones,
+                    cuantos: conteo
+                })
+            })
+        })
+
+})
+
+// Crear una verificacion
+app.post('/verificacion', (req, res) => {
+    let body = req.body
+
+    let verificaion = new Verificacion({
+        auditor: body.auditor,
+        auditoria: body.auditoria,
+        entrevistado: body.entrevistado,
+        puntoNorma: body.puntoNorma,
+        pregunta: body.pregunta,
+        documento: body.documento,
+        evidencia: body.evidencia,
+        hallazgos: body.hallazgos,
+        fecha: body.fecha
+    })
+})
+
 
 
 
