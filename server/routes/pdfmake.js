a3896a92bbf237d8c99b5d6184bf3ad529da3d17
Jorge Madera <decoy33tnt@hotmail.com>
2019-10-08 11:17:54 -0500

    cambios en pdf planeacion

--- server/routes/pdfmake.js
+++ server/routes/pdfmake.js
@@ -1,18 +1,43 @@
 const express = require('express');
 const app = express();
 
-const pdfMake = require('../../pdfmake/pdfmake');
-const vfsFonts = require('../../pdfmake/vfs_fonts');
-
-const Institucion = require('../models/institucion')
-const InstitucionRutas = require('../routes/institucion')
+const pdfMake = require('../pdfmake/pdfmake');
+const vfsFonts = require('../pdfmake/vfs_fonts');
 
 pdfMake.vfs = vfsFonts.pdfMake.vfs;
 
 app.post('/pdf/planeacion', (req, res, next) => {
     //res.send('PDF');
-    let body = req.body
-    var fecha = { text: 'FECHA:', margin: [35, 20, 0, 0], style: 'tableHeader' }
+    let body = req.body;
+    var hora = "Mickey";
+
+    var tablas = [];
+
+    var arrBody = [
+        [
+            [{ text: 'HORARIO', fillColor: '#dddddd', alignment: 'center', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: 'PROCESO / ACTIVIDAD-REQUISITO / CRITERIO', fillColor: '#dddddd', alignment: 'center', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: 'PARTICIPANTES', fillColor: '#dddddd', alignment: 'center', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: 'CONTACTO', fillColor: '#dddddd', fontSize: 10, alignment: 'center', colSpan: 1, style: 'tableHeader' }, { text: 'ÁREA / SITIO', fillColor: '#dddddd', alignment: 'center', fontSize: 10, colSpan: 1, style: 'tableHeader' }],
+            [{ text: hora, fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: ' ', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: ' ', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: ' ', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: ' ', fontSize: 10, colSpan: 1, style: 'tableHeader' }],
+        ],
+        [
+            [{ text: ' ', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: ' ', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: ' ', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: ' ', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: ' ', fontSize: 10, colSpan: 1, style: 'tableHeader' }],
+        ],
+        [
+            [{ text: 'HORARIO', fillColor: '#dddddd', alignment: 'center', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: 'PROCESO / ACTIVIDAD-REQUISITO / CRITERIO', fillColor: '#dddddd', alignment: 'center', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: 'PARTICIPANTES', fillColor: '#dddddd', alignment: 'center', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: 'CONTACTO', fillColor: '#dddddd', fontSize: 10, alignment: 'center', colSpan: 1, style: 'tableHeader' }, { text: 'ÁREA / SITIO', fillColor: '#dddddd', alignment: 'center', fontSize: 10, colSpan: 1, style: 'tableHeader' }],
+            [{ text: ' ', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: ' ', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: ' ', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: ' ', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: ' ', fontSize: 10, colSpan: 1, style: 'tableHeader' }],
+        ]
+    ];
+
+    //Ciclo para crear las fechas y tablas dependiendo de la planeacion
+    for (let index = 0; index < 3; index++) {
+        tablas.push({ text: 'FECHA:', margin: [35, 20, 0, 0], style: 'tableHeader' }, {
+            style: 'tableExample',
+            table: {
+                widths: [50, 'auto', 'auto', 'auto', 'auto'],
+                body: arrBody[index],
+
+            }
+        });
+    }
 
     var documentDefinition = {
         content: [{
@@ -21,13 +46,9 @@ app.post('/pdf/planeacion', (req, res, next) => {
                     heights: [50],
                     widths: [500],
                     body: [
-                        [{ text: 'PLAN DE AUDITORIA PARA EL SGI DEL G3', margin: [10, 15, 10, 10], alignment: 'center' }]
+                        [{ text: 'PLAN DE AUDITORIA PARA EL SGI DEL G3', color: 'gray', margin: [10, 15, 10, 10], alignment: 'center' }]
                     ]
                 }
-                /*  stack: [
-                      'PLAN DE AUDITORIA PARA EL SGI DEL G3',
-                  ], 
-                  style: 'header' */
             },
             {
                 style: 'tableExample',
@@ -36,46 +57,14 @@ app.post('/pdf/planeacion', (req, res, next) => {
                     widths: [140, 220, 38, 50],
                     body: [
                         [{ text: 'Instituto Tecnologico Superior:', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: body.institucion, colSpan: 3, style: 'tableHeader', alignment: 'center' }, ' ', ' '],
-                        [{ text: 'Norma de Referencia:', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: ' ', colSpan: 3, style: 'tableHeader', alignment: 'center' }, ' ', ' '],
-                        [{ text: 'Domicilio:', fontSize: 10, colSpan: 1, style: 'tableHeader' }, ' ', { text: 'Idioma:', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: 'Español', fontSize: 10, colSpan: 1, style: 'tableHeader', alignment: 'center' }],
-                        [{ text: 'Objetivo:', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: ' ', colSpan: 1, style: 'tableHeader', alignment: 'center' }, { text: 'NACE:', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: '37', fontSize: 10, colSpan: 1, style: 'tableHeader', alignment: 'center' }],
-                        [{ text: 'Alcance:', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: ' ', colSpan: 3, style: 'tableHeader', alignment: 'center' }, ' ', ' '],
+                        [{ text: 'Norma de Referencia:', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: body.normasR, colSpan: 3, style: 'tableHeader', alignment: 'center' }, ' ', ' '],
+                        [{ text: 'Domicilio:', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: body.domicilio, fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: 'Idioma:', fontSize: 10, colSpan: 1, style: 'tableHeader', alignment: 'center' }, { text: 'Español', fontSize: 10, colSpan: 1, style: 'tableHeader', alignment: 'center' }],
+                        [{ text: 'Objetivo:', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: body.objetivo, colSpan: 1, style: 'tableHeader', alignment: 'center' }, { text: 'NACE:', fontSize: 10, colSpan: 1, style: 'tableHeader', alignment: 'center' }, { text: '37', fontSize: 10, colSpan: 1, style: 'tableHeader', alignment: 'center' }],
+                        [{ text: 'Alcance:', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: body.alcance, colSpan: 3, style: 'tableHeader', alignment: 'center' }, ' ', ' '],
                     ]
                 }
             },
-            fecha,
-            { fecha },
-            { fecha },
-            // { text: 'FECHA:', margin: [35, 20, 0, 0], style: 'tableHeader' },
-            {
-                style: 'tableExample', // optional
-                table: {
-                    widths: [50, 'auto', 'auto', 'auto', 'auto'],
-                    body: [
-                        [{ text: 'HORARIO', alignment: 'center', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: 'PROCESO / ACTIVIDAD-REQUISITO / CRITERIO', alignment: 'center', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: 'PARTICIPANTES', alignment: 'center', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: 'CONTACTO', fontSize: 10, alignment: 'center', colSpan: 1, style: 'tableHeader' }, { text: 'ÁREA / SITIO', alignment: 'center', fontSize: 10, colSpan: 1, style: 'tableHeader' }],
-                        [' ', ' ', ' ', ' ', ' '],
-                        [' ', ' ', ' ', ' ', ' '],
-                        [' ', ' ', ' ', ' ', ' '],
-                        [' ', ' ', ' ', ' ', ' '],
-                        [' ', ' ', ' ', ' ', ' '],
-                    ]
-                }
-            },
-            { text: 'FECHA:', margin: [35, 20, 0, 0], style: 'tableHeader' },
-            {
-                style: 'tableExample', // optional
-                table: {
-                    widths: [50, 'auto', 'auto', 'auto', 'auto'],
-                    body: [
-                        [{ text: 'HORARIO', alignment: 'center', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: 'PROCESO / ACTIVIDAD-REQUISITO / CRITERIO', alignment: 'center', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: 'PARTICIPANTES', alignment: 'center', fontSize: 10, colSpan: 1, style: 'tableHeader' }, { text: 'CONTACTO', fontSize: 10, alignment: 'center', colSpan: 1, style: 'tableHeader' }, { text: 'ÁREA / SITIO', alignment: 'center', fontSize: 10, colSpan: 1, style: 'tableHeader' }],
-                        [' ', ' ', ' ', ' ', ' '],
-                        [' ', ' ', ' ', ' ', ' '],
-                        [' ', ' ', ' ', ' ', ' '],
-                        [' ', ' ', ' ', ' ', ' '],
-                        [' ', ' ', ' ', ' ', ' '],
-                    ]
-                }
-            },
+            tablas,
             {
                 style: 'firma',
                 table: {
@@ -107,15 +96,20 @@ app.post('/pdf/planeacion', (req, res, next) => {
                 margin: [10, 5, 0, 15]
             },
             titulo: {
-
+                bold: true,
                 fontSize: 20,
                 margin: [5, 5, 5, 25]
             },
             firma: {
-                margin: [170, 5, 0, 15]
+                margin: [170, 20, 0, 15]
             },
             tableHeader: {
                 bold: true,
+                color: 'black'
+            },
+            tableHeader2: {
+                bold: true,
+                widths: [100, 220, 38, 50],
                 color: 'black'
             }
         }
@@ -125,14 +119,11 @@ app.post('/pdf/planeacion', (req, res, next) => {
     pdfDoc.getBase64((data) => {
         res.writeHead(200, {
             'Content-Type': 'application/pdf',
-            'Content-Disposition': 'attachment;filename="filename.pdf"'
+            'Content-Disposition': 'attachment;filename="plan_auditoria.pdf"'
         });
 
         const download = Buffer.from(data.toString('utf-8'), 'base64');
         res.end(download);
     });
-
 });
-
-
 module.exports = app;
