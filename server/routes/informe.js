a18cd5d71a20e228f543701509fbf03d60dcf33d
Jorge Madera <decoy33tnt@hotmail.com>
2019-10-21 13:26:19 -0500

    rutas para cambiar comentarios y conclusiones en informe

--- server/routes/informe.js
+++ server/routes/informe.js
@@ -173,4 +173,56 @@ app.put('/informe/oportunidades/:id', (req, res) => {
     })
 })
 
+app.put('/informe/comentarios/:id', (req, res) => {
+    let id = req.params.id
+    let body = _.pick(req.body, ['comentarios'])
+
+    Informe.findByIdAndUpdate(id, body, { new: true, runValidators: true, context: 'query' }, (err, informeDB) => {
+        if (err) {
+            res.status(500).json({
+                ok: false,
+                err
+            })
+        }
+
+        if (!informeDB) {
+            res.status(400).json({
+                ok: false,
+                err
+            })
+        }
+
+        res.json({
+            ok: true,
+            informeDB
+        })
+    })
+})
+
+app.put('/informe/conclusiones/:id', (req, res) => {
+    let id = req.params.id
+    let body = _.pick(req.body, ['conclusiones'])
+
+    Informe.findByIdAndUpdate(id, body, { new: true, runValidators: true, context: 'query' }, (err, informeDB) => {
+        if (err) {
+            res.status(500).json({
+                ok: false,
+                err
+            })
+        }
+
+        if (!informeDB) {
+            res.status(400).json({
+                ok: false,
+                err
+            })
+        }
+
+        res.json({
+            ok: true,
+            informeDB
+        })
+    })
+})
+
 module.exports = app
