cf1626566e7177a3408191eb764d361e1a57dcf6
Jorge Madera <decoy33tnt@hotmail.com>
2019-10-31 11:17:11 -0600

    Cambio de bases de datos

--- server/config/config.js
+++ server/config/config.js
@@ -31,8 +31,8 @@ let urlDB
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/sgi'
 } else {
-    urlDB = process.env.MONGO_URI
-        // urlDB = process.env.MONGO_Prueba
+    urlDB = process.env.MONGO_Prueba
+        // urlDB = process.env.MONGO_URI
         // urlDB = 'mongodb+srv://Administrador:MFoqRpyLvLvTfY15@cluster0-4zhf2.mongodb.net/pruebaRespaldo?retryWrites=true&w=majority'
 }
 
